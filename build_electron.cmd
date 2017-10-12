2fdd61f5c68ffc517506780812a3577139a5c301
Fran Carrascosa <fran@metrica-sports.com>
2017-10-12 18:37:14 +0200

    Using VLC 64-bits- version. Update Electron version.

--- build_electron.cmd
+++ build_electron.cmd
@@ -1,5 +1,5 @@
 set npm_config_wcjs_runtime="electron"
-set npm_config_wcjs_runtime_version="0.33.9"
-set npm_config_wcjs_arch="ia32"
+set npm_config_wcjs_runtime_version="1.6.11"
+set npm_config_wcjs_arch="x64"
 
 npm install
