540f5ebcfda29326e5059475c2a2a52e455288ca
Fran Carrascosa <fran@metrica-sports.com>
2017-10-30 19:21:00 +0100

    Electron version increased in building scripts

--- build_electron.cmd
+++ build_electron.cmd
@@ -1,5 +1,5 @@
 set npm_config_wcjs_runtime="electron"
-set npm_config_wcjs_runtime_version="1.6.11"
+set npm_config_wcjs_runtime_version="1.7.9"
 set npm_config_wcjs_arch="x64"
 
 npm install
--- build_electron.sh
+++ build_electron.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 export npm_config_wcjs_runtime="electron"
-export npm_config_wcjs_runtime_version="1.6.11"
+export npm_config_wcjs_runtime_version="1.7.9"
 
 npm install
