506c0eb91b1f44e3f3372a307cfb3e937fe2f342
FranCarrascosaMS <fran@metrica-sports.com>
2019-10-31 19:12:55 +0100

    Display frame for debugging with OpenCV

--- test.bat
+++ test.bat
@@ -3,4 +3,5 @@
 set script_path=%~dp0
 set VLC_PLUGIN_PATH=%script_path%bin\plugins
 
+rem call build_node
 npm test
--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+const cv = require('opencv4nodejs');
 const fileUrl = require('file-url');
 const path = require('path');
 const webChimera = require('../bin/WebChimera.js.node');
@@ -13,8 +14,11 @@ const play = false;
 const playReverse = false;
 const time = 0;
 vlc.load(videoPath, play, playReverse, time);
-vlc.play();
 
 function onFrameReady(videoFrame, frame, time) {
   console.log(`onFrameReady: frame ${frame} - time ${time}ms`);
+
+  const mat = new cv.Mat(videoFrame, videoFrame.height, videoFrame.width, cv.CV_8UC4);
+  cv.imshow('frame', mat);
+  cv.waitKey(1);
 }
